fdaf6d7f4dda4f1c9fb07bbc409ca3db439a8fe7
mohsinzafaruk1996 <33597363+mohsinzafaruk1996@users.noreply.github.com>
2017-11-13 13:24:41 +0000

    Update lessthan5.py

--- lessthan5.py
+++ lessthan5.py
@@ -3,3 +3,14 @@ a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 for element in a:
   if element < 5:
     print(element)
+    
+newlist = []
+
+for element in a:
+  if element < 5:
+    newlist.append(element)
+    
+print(newlist)
+  
+
+    
