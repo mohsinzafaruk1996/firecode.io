6050db73c48bc51b8c2753f61fd1cd25117d5a03
mohsinzafaruk1996 <33597363+mohsinzafaruk1996@users.noreply.github.com>
2017-11-14 12:41:27 +0000

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -17,4 +17,14 @@ if a1 == a[0:int(len(a)/2)+1] :
   print("It's a palindrome  " + a)
   
   
+    
+wrd=input("Please enter a word")
+wrd=str(wrd)
+rvs=wrd[::-1]
+print(rvs)
+if wrd == rvs:
+    print("This word is a palindrome")
+else:
+    print("This word is not a palindrome")  
+  
   
